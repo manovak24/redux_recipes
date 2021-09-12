0b49dc2a220c2f0ff950490a08ca1996867e702a
emagdnimMQT <mark.w.novak@gmail.com>
2021-09-12 10:36:12 -0400

    Refractored code by using redux toolkit.

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,14 @@
-import { createStore, combineReducers } from 'redux';
-import {favoriteRecipesSlice} from '../features/favoriteRecipes/favoriteRecipesSlice.js';
-import {searchTermReducer} from '../features/searchTerm/searchTermSlice.js';
-import {allRecipesReducer} from '../features/allRecipes/allRecipesSlice.js';
+import { configureStore } from '@reduxjs/toolkit';
+import favoriteRecipesReducer from '../features/favoriteRecipes/favoriteRecipesSlice.js';
+import searchTermReducer from '../features/searchTerm/searchTermSlice.js';
+import allRecipesReducer from '../features/allRecipes/allRecipesSlice.js';
 
-export const store = createStore(combineReducers({
-  favoriteRecipes: favoriteRecipesSlice.reducer,
-  searchTerm: searchTermReducer,
-  allRecipes: allRecipesReducer
-}));
+export default configureStore({
+  reducer: {
+    favoriteRecipes: favoriteRecipesReducer,
+    searchTerm: searchTermReducer,
+    allRecipes: allRecipesReducer
+  }
+})
+
+  
--- src/features/favoriteRecipes/FavoriteRecipesSlice.js
+++ src/features/favoriteRecipes/FavoriteRecipesSlice.js
@@ -1,13 +1,14 @@
 import { createSlice } from '@reduxjs/toolkit';
 import { selectSearchTerm } from '../searchTerm/searchTermSlice.js';
 
-/* Create your Slice object here. */
 const options = {
   name: 'favoriteRecipes',
   initialState: [],
   reducers: {
     addRecipe: (state, action) => {
-      return [...state, action.payload]
+      state.push({
+        ...action.payload
+      })
     },
     removeRecipe: (state, action) => {
       return state.filter(recipe => recipe.id !== action.payload.id)
@@ -16,8 +17,6 @@ const options = {
 };
 
 export const favoriteRecipesSlice = createSlice(options);
-
-/* Do not delete the code below...*/
 
 export const selectFavoriteRecipes = (state) => state.favoriteRecipes;
 
@@ -29,3 +28,7 @@ export const selectFilteredFavoriteRecipes = (state) => {
     recipe.name.toLowerCase().includes(searchTerm.toLowerCase())
   );
 };
+
+export const {addRecipe, removeRecipe} = favoriteRecipesSlice.actions;
+console.log(favoriteRecipesSlice);
+export default favoriteRecipesSlice.reducer;
